7ae69c52505251aa3f5fd351f142c5a51a48d26c
Matus Backor <MatusB@kentico.com>
2017-12-06 14:29:00 +0100

    KA-233 Add comparer for Items

---
+++ TodoApp/TodoApp.Api.Tests/Comparer/ItemEqualityComparer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Tests.Comparer
+{
+    internal static class ItemEqualityComparerWrapper
+    {
+        private static Lazy<ItemEqualityComparer> Lazy => new Lazy<ItemEqualityComparer>();
+
+        private static ItemEqualityComparer Comparer => Lazy.Value;
+
+        private sealed class ItemEqualityComparer : IEqualityComparer<Item>
+        {
+            public bool Equals(Item x, Item y)
+            {
+                if (x == null && y == null)
+                {
+                    return true;
+                }
+                if (x == null | y == null)
+                {
+                    return false;
+                }
+                return x.Text == y.Text && x.Id == y.Id;
+            }
+
+            public int GetHashCode(Item obj)
+            {
+                return obj.Text.GetHashCode() ^ obj.Id.GetHashCode();
+            }
+        }
+
+        public static EqualConstraint UsingItemEqualityComparer(this EqualConstraint constraint)
+            => constraint.Using(Comparer);
+    }
+}
