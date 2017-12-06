b8f7b843414e842e0db45cc5de79bc1cb1f209fa
Matus Backor <MatusB@kentico.com>
2017-12-06 17:25:53 +0100

    KA-233 Replace array with iterator

--- TodoApp/TodoApp.Api/Controllers/ItemsController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemsController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -8,15 +10,22 @@ namespace TodoApp.Api.Controllers
 {
     public class ItemsController : ApiController
     {
-        private static readonly Item[] Items = new Item[3]
+        private static readonly Item ItemToPost =
+            new Item("itemToPost", new Guid("e6eb4638-38a4-49ac-8aaf-878684397707"));
+
+        private static IEnumerable<Item> IteratedItems
         {
-            new Item("item0", new Guid("e6eb4638-38a4-49ac-8aaf-878684397702")),
-            new Item("item1", new Guid("a5d4b549-bdd3-4ec2-8210-ff42926aa141")),
-            new Item("item2", new Guid("45c4fb8b-1cdf-42ca-8a61-67fd7f781057"))
-        };
+            get
+            {
+                yield return new Item("item0", new Guid("e6eb4638-38a4-49ac-8aaf-878684397702"));
+                yield return new Item("item1", new Guid("a5d4b549-bdd3-4ec2-8210-ff42926aa141"));
+                yield return new Item("item2", new Guid("45c4fb8b-1cdf-42ca-8a61-67fd7f781057"));
+            }
+        }
+
+        private static readonly Item[] Items = IteratedItems.ToArray();
 
         // GET api/<controller>
-
         public async Task<IHttpActionResult> GetAsync()
             => await Task.FromResult(Ok(Items));
 
@@ -26,14 +35,14 @@ namespace TodoApp.Api.Controllers
 
         // POST api/<controller>
         public async Task<IHttpActionResult> PostAsync([FromBody] Item item)
-            => await Task.FromResult(CreatedAtRoute("DefaultApi", new {Id = 5}, item));
+            => await Task.FromResult(Created(new Uri("http://localhost:55557/api/items"), ItemToPost));
 
         // PUT api/<controller>/5
         public async Task<IHttpActionResult> PutAsync(Guid id, [FromBody] Item item)
             => await Task.FromResult(Content(HttpStatusCode.Accepted, Items[1]));
 
         // DELETE api/<controller>/5
-        public async Task<IHttpActionResult> DeleteAsync(Guid id) 
+        public async Task<IHttpActionResult> DeleteAsync(Guid id)
             => await Task.FromResult(Ok());
     }
 }
