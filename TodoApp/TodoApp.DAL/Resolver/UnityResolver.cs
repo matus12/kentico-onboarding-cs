2fc66e6f100f6183eb33fbce9c1bb2eabc653f50
Matus Backor <MatusB@kentico.com>
2017-12-08 09:45:28 +0100

    KA-234 Add UnityResolver

---
+++ TodoApp/TodoApp.DAL/Resolver/UnityResolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Unity;
+using Unity.Exceptions;
+
+namespace TodoApp.DAL.Resolver
+{
+    public class UnityResolver: IDependencyResolver
+    {
+        protected IUnityContainer Container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            Container = container ?? throw new ArgumentNullException(nameof(container));
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return Container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return Container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = Container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            Container.Dispose();
+        }
+    }
+}
