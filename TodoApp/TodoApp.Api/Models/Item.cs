ed6d450914579c40d73b2a9e08c413444b2f773e
Matus Backor <MatusB@kentico.com>
2017-12-11 13:21:58 +0100

    KA-233 Remove get/set from properties

--- TodoApp/TodoApp.Api/Models/Item.cs
+++ TodoApp/TodoApp.Api/Models/Item.cs
@@ -4,7 +4,7 @@ namespace TodoApp.Api.Models
 {
     public class Item
     {
-        public Guid Id { get; set; }
-        public string Text { get; set; }
+        public Guid Id;
+        public string Text;
     }
 }
