0ada6f3d4b6f1b48f9f37bf981027b907c6489af
Matus Backor <MatusB@kentico.com>
2017-12-14 13:30:15 +0100

    KA-234 Restructure solution

--- TodoApp/src/TodoApp.Database/Properties/AssemblyInfo.cs
+++ TodoApp/src/TodoApp.Database/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TodoApp.DAL2")]
+[assembly: AssemblyTitle("TodoApp.Database")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kentico software s.r.o.")]
-[assembly: AssemblyProduct("TodoApp.DAL2")]
+[assembly: AssemblyProduct("TodoApp.Database")]
 [assembly: AssemblyCopyright("Copyright © Kentico software s.r.o. 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
