ad530f5b72739f6d64db82053e52c0a490b103ae
Matus Backor <MatusB@kentico.com>
2017-12-18 16:26:52 +0100

    KA-234 Add tests for location helper

---
+++ TodoApp/test/TodoApp.Api.Tests/Helpers/LocationHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+using NSubstitute;
+
+namespace TodoApp.Api.Tests.Helpers
+{
+    [TestFixture]
+    internal class LocationHelperTests
+    {
+        [Test]
+        public void GetUriLocation_newId_ReturnsCorrectUri()
+        {
+            var requestMessage = Substitute.For<HttpRequestMessage>();
+            var locationHelper = new LocationHelper(requestMessage);
+            var newId = new Guid("5f1570b2-9e59-4281-9bf2-d5ee136ebf21");
+            var expectedUri = new Uri("/api/v1/items/5f1570b2-9e59-4281-9bf2-d5ee136ebf21", UriKind.Relative);
+
+            var result = locationHelper.GetUriLocation(newId);
+
+            Assert.That(result, Is.EqualTo(expectedUri));
+        }
+    }
+}
