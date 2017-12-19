04c0f692ff161f2e7bb4ad608ea938c612d462a9
Matus Backor <MatusB@kentico.com>
2017-12-19 09:43:59 +0100

    KA-234 Fix tests for LocationHelper

--- TodoApp/test/TodoApp.Api.Tests/Helpers/LocationHelperTests.cs
+++ TodoApp/test/TodoApp.Api.Tests/Helpers/LocationHelperTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
 using NSubstitute;
 
 namespace TodoApp.Api.Tests.Helpers
@@ -12,6 +14,12 @@ namespace TodoApp.Api.Tests.Helpers
         public void GetUriLocation_newId_ReturnsCorrectUri()
         {
             var requestMessage = Substitute.For<HttpRequestMessage>();
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.Routes.MapHttpRoute(
+                RoutesConfig.ApiV1Route,
+                "api/v1/items/{id}"
+            );
+            requestMessage.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, httpConfiguration);
             var locationHelper = new LocationHelper(requestMessage);
             var newId = new Guid("5f1570b2-9e59-4281-9bf2-d5ee136ebf21");
             var expectedUri = new Uri("/api/v1/items/5f1570b2-9e59-4281-9bf2-d5ee136ebf21", UriKind.Relative);
