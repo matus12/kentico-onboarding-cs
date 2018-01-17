745442b6df72057dddba55ebd946907954a46f43
Matus Backor <MatusB@kentico.com>
2018-01-17 13:48:00 +0100

    KA-234 Refactor ItemsControllerTests

--- TodoApp/test/TodoApp.Api.Tests/Controllers/ItemsControllerTests.cs
+++ TodoApp/test/TodoApp.Api.Tests/Controllers/ItemsControllerTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -19,37 +17,29 @@ namespace TodoApp.Api.Tests.Controllers
     [TestFixture]
     internal class ItemsControllerTests
     {
-        private ItemsController _controller;
-        private IItemRepository _repository;
-        private ILocationHelper _helper;
-        private static readonly Guid Guid0 = new Guid("e6eb4638-38a4-49ac-8aaf-878684397702");
-        private static readonly Guid Guid1 = new Guid("a5d4b549-bdd3-4ec2-8210-ff42926aa141");
-        private static readonly Guid Guid2 = new Guid("45c4fb8b-1cdf-42ca-8a61-67fd7f781057");
-
-        private static readonly Item ItemToPost =
-            new Item {Text = "itemToPost", Id = new Guid("e6eb4638-38a4-49ac-8aaf-878684397707")};
-
         private const string UriString = "/e6eb4638-38a4-49ac-8aaf-878684397707/test-route/15";
 
         private static readonly Uri Uri = new Uri(UriString, UriKind.Relative);
+        private static readonly Guid Guid0 = new Guid("e6eb4638-38a4-49ac-8aaf-878684397702");
+        private static readonly Guid Guid1 = new Guid("a5d4b549-bdd3-4ec2-8210-ff42926aa141");
+        private static readonly Guid Guid2 = new Guid("45c4fb8b-1cdf-42ca-8a61-67fd7f781057");
+        private static readonly Item ItemToPost =
+            new Item { Text = "itemToPost", Id = new Guid("e6eb4638-38a4-49ac-8aaf-878684397707") };
 
-        private readonly Item[] _items = IteratedItems.ToArray();
-
-        private static IEnumerable<Item> IteratedItems
+        private readonly Item[] _items =
         {
-            get
-            {
-                yield return new Item {Text = "item0", Id = Guid0};
-                yield return new Item {Text = "item1", Id = Guid1};
-                yield return new Item {Text = "item2", Id = Guid2};
-            }
-        }
+            new Item {Text = "item0", Id = Guid0},
+            new Item {Text = "item1", Id = Guid1},
+            new Item {Text = "item2", Id = Guid2},
+        };
+
+        private ItemsController _controller;
+        private IItemRepository _repository;
+        private ILocationHelper _helper;
 
         [SetUp]
         public void SetUp()
         {
-            var config = new HttpConfiguration();
-
             _repository = Substitute.For<IItemRepository>();
 
             _helper = Substitute.For<ILocationHelper>();
@@ -57,7 +47,7 @@ namespace TodoApp.Api.Tests.Controllers
             _controller = new ItemsController(_repository, _helper)
             {
                 Request = new HttpRequestMessage(),
-                Configuration = config
+                Configuration = new HttpConfiguration()
             };
         }
 
@@ -66,13 +56,11 @@ namespace TodoApp.Api.Tests.Controllers
         {
             _repository.GetAllAsync().Returns(_items);
 
-            var actionResult = await _controller.GetAsync();
-            var contentResult = await actionResult.ExecuteAsync(CancellationToken.None);
-            contentResult.TryGetContentValue(out Item[] value);
+            var (contentResult, items) = await GetResultFromAction<Item[]>(controller => controller.GetAsync());
 
             Assert.That(contentResult.Content, Is.Not.Null);
             Assert.That(contentResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(value, Is.EqualTo(_items).AsCollection.UsingItemEqualityComparer());
+            Assert.That(items, Is.EqualTo(_items).AsCollection.UsingItemEqualityComparer());
         }
 
         [Test]
@@ -81,7 +69,7 @@ namespace TodoApp.Api.Tests.Controllers
             _repository.GetByIdAsync(Arg.Any<Guid>()).Returns(_items[1]);
             _repository.GetByIdAsync(Guid0).Returns(_items[0]);  
 
-            var (contentResult, item) = await GetResultFromAction(await _controller.GetAsync(Guid0));
+            var (contentResult, item) = await GetResultFromAction<Item>(controller => controller.GetAsync(Guid0));
 
             Assert.That(contentResult.Content, Is.Not.Null);
             Assert.That(contentResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -94,7 +82,7 @@ namespace TodoApp.Api.Tests.Controllers
             _repository.AddAsync(ItemToPost).ReturnsForAnyArgs(ItemToPost);
             _helper.GetUriLocation(Arg.Any<Guid>()).Returns(Uri);
 
-            var (createdResult, item) = await GetResultFromAction(await _controller.PostAsync(ItemToPost));
+            var (createdResult, item) = await GetResultFromAction<Item>(controller => controller.PostAsync(ItemToPost));
             var location = createdResult.Headers.Location.ToString();
 
             Assert.That(createdResult.StatusCode, Is.EqualTo(HttpStatusCode.Created));
@@ -106,7 +94,8 @@ namespace TodoApp.Api.Tests.Controllers
         public async Task PutAsync_ItemToUpdateWithExistingId_ReturnsUpdatedItem()
         {
             _repository.UpdateAsync(_items[0]).ReturnsForAnyArgs(_items[1]);
-            var (contentResult, item) = await GetResultFromAction(await _controller.PutAsync(_items[1].Id, _items[1]));
+
+            var (contentResult, item) = await GetResultFromAction<Item>(controller => controller.PutAsync(_items[1].Id, _items[1]));
 
             Assert.That(contentResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             Assert.That(item, Is.EqualTo(_items[1]).UsingItemEqualityComparer());
@@ -115,18 +104,24 @@ namespace TodoApp.Api.Tests.Controllers
         [Test]
         public async Task DeleteAsync_ExistingId_ReturnsNoContent()
         {
-            var actionResult = await _controller.DeleteAsync(Guid0);
-            var result = await actionResult.ExecuteAsync(CancellationToken.None);
+            var response = await GetResultFromAction(controller => controller.DeleteAsync(Guid0));
 
             await _repository.Received(1).DeleteAsync(Arg.Any<Guid>());
-            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
         }
 
-        private static async Task<(HttpResponseMessage message, Item item)> GetResultFromAction(IHttpActionResult actionResult)
+        private async Task<(HttpResponseMessage message, TPayload item)> GetResultFromAction<TPayload>(Func<ItemsController, Task<IHttpActionResult>> actionSelector)
         {
-            var responseMessage = await actionResult.ExecuteAsync(CancellationToken.None);
-            Console.WriteLine(responseMessage.TryGetContentValue(out Item item));
+            var responseMessage = await GetResultFromAction(actionSelector);
+            responseMessage.TryGetContentValue(out TPayload item);
             return (responseMessage, item);
+        }
+
+        private async Task<HttpResponseMessage> GetResultFromAction(Func<ItemsController, Task<IHttpActionResult>> actionSelector)
+        {
+            var actionResult = await actionSelector(_controller);
+            var responseMessage = await actionResult.ExecuteAsync(CancellationToken.None);
+            return responseMessage;
         }
     }
 }
