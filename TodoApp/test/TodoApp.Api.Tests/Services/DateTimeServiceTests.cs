b2dadb85257e4e3711cbb5bdb1bde8b2b07f89a7
Matus Backor <MatusB@kentico.com>
2018-01-26 12:40:31 +0100

    KA-235 Improve DateTimeService tests

--- TodoApp/test/TodoApp.Api.Tests/Services/DateTimeServiceTests.cs
+++ TodoApp/test/TodoApp.Api.Tests/Services/DateTimeServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using NUnit.Framework;
 using TodoApp.Contracts.Services;
 using TodoApp.Services;
@@ -9,26 +11,36 @@ namespace TodoApp.Api.Tests.Services
     {
         private IDateTimeService _dateTimeService;
 
+        private static IEnumerable<DateTime> MaxMinDateTimes
+        {
+            get
+            {
+                yield return DateTime.MaxValue;
+                yield return DateTime.MinValue;
+            }
+        }
+
         [SetUp]
         public void SetUp()
         {
             _dateTimeService = new DateTimeService();    
         }
 
-        [Test]
-        public void GetCurrentDateTime_ReturnsCurrentDateAndTime()
+        [TestCaseSource(nameof(MaxMinDateTimes))]
+        public void GetCurrentDateTime_ReturnsCorrectDateTime(DateTime testTime)
         {
-            var expectedDateTime = DateTime.Now;
-
             var time = _dateTimeService.GetCurrentDateTime();
 
-            Assert.That(time.Ticks - expectedDateTime.Ticks, Is.LessThan(50000));
+            Assert.That(time.Ticks, Is.Not.EqualTo(testTime).Within(50000));
         }
 
         [Test]
         public void GetCurrentDateTime_ReturnsDifferentTimeWhenCalledTwice()
         {
+            var pause = new ManualResetEvent(false);
+
             var time0 = _dateTimeService.GetCurrentDateTime();
+            pause.WaitOne(1000);
             var time1 = _dateTimeService.GetCurrentDateTime();
 
             Assert.That(time1, Is.GreaterThan(time0));
