2d5a07ae89136b43d00bc4604f25f29db00e66b0
Matus Backor <MatusB@kentico.com>
2018-02-12 10:58:47 +0100

    KA-235 Filter exceptions in UnityResolver

--- TodoApp/src/TodoApp.Api/Resolver/UnityResolver.cs
+++ TodoApp/src/TodoApp.Api/Resolver/UnityResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http.Dependencies;
 using Unity;
 using Unity.Exceptions;
@@ -8,6 +9,32 @@ namespace TodoApp.Api.Resolver
 {
     internal sealed class UnityResolver : IDependencyResolver
     {
+        private const string HostBufferPolicySelectorException = "System.Web.Http.Hosting.IHostBufferPolicySelector";
+        private const string HttpControllerSelectorException = "System.Web.Http.Dispatcher.IHttpControllerSelector";
+        private const string HttpControllerActivatorException = "System.Web.Http.Dispatcher.IHttpControllerActivator";
+        private const string HttpActionSelectorException = "System.Web.Http.Controllers.IHttpActionSelector";
+        private const string HttpActionInvokerException = "System.Web.Http.Controllers.IHttpActionInvoker";
+        private const string ExceptionHandlerException = "System.Web.Http.ExceptionHandling.IExceptionHandler";
+        private const string ContentNegotiatorException = "System.Net.Http.Formatting.IContentNegotiator";
+        private const string ModelMetadataProviderException = "System.Web.Http.Metadata.ModelMetadataProvider";
+        private const string ModelValidatorCacheException = "System.Web.Http.Validation.IModelValidatorCache";
+
+        private static IEnumerable<string> Exceptions
+        {
+            get
+            {
+                yield return HostBufferPolicySelectorException;
+                yield return HttpControllerSelectorException;
+                yield return HttpControllerActivatorException;
+                yield return HttpActionSelectorException;
+                yield return HttpActionInvokerException;
+                yield return ExceptionHandlerException;
+                yield return ContentNegotiatorException;
+                yield return ModelMetadataProviderException;
+                yield return ModelValidatorCacheException;
+            }
+        }
+
         private readonly IUnityContainer _container;
         private bool _disposed;
 
@@ -23,13 +50,13 @@ namespace TodoApp.Api.Resolver
                 return _container.Resolve(serviceType);
             }
             catch (ResolutionFailedException)
-                when (serviceType?.FullName?.StartsWith("TodoApp") == true)
+                when (Exceptions.Contains(serviceType?.FullName))
             {
-                throw;
+                return null;
             }
             catch (ResolutionFailedException)
             {
-                return null;
+                throw;
             }
         }
 
@@ -40,8 +67,13 @@ namespace TodoApp.Api.Resolver
                 return _container.ResolveAll(serviceType);
             }
             catch (ResolutionFailedException)
+                when (Exceptions.Contains(serviceType?.FullName))
             {
                 return new List<object>();
+            }
+            catch (ResolutionFailedException)
+            {
+                throw;
             }
         }
 
