efeb24e8f782666af92d56cce7517f1e4d8bf58a
Matus Backor <MatusB@kentico.com>
2018-02-21 15:07:07 +0100

    KA-236 Fix failing tests due to update refactor

--- TodoApp/test/TodoApp.Services.Tests/Services/UpdateItemServiceTests.cs
+++ TodoApp/test/TodoApp.Services.Tests/Services/UpdateItemServiceTests.cs
@@ -17,48 +17,69 @@ namespace TodoApp.Services.Tests.Services
         private IUpdateItemService _updateItemService;
         private ITimeService _timeService;
         private IItemRepository _repository;
+        private IGetItemByIdService _getItemByIdService;
 
         [SetUp]
         public void SetUp()
         {
             _repository = Substitute.For<IItemRepository>();
             _timeService = Substitute.For<ITimeService>();
+            _getItemByIdService = Substitute.For<IGetItemByIdService>();
 
-            _updateItemService = new UpdateItemService(_repository, _timeService);
+            _updateItemService = new UpdateItemService(_repository, _timeService, _getItemByIdService);
         }
 
         [Test]
         public async Task UpdateItem_ItemToUpdate_ReturnsUpdatedItem()
         {
+            const string originalText = "originalText";
             const string updatedText = "updatedText";
             var creationTime = new DateTime(2012, 2, 5, 13, 0, 14);
             var currentTime = new DateTime(2018, 2, 5, 13, 0, 14);
             _timeService.GetCurrentDateTime().Returns(currentTime);
             var guid = new Guid("6548b7f6-d35c-4075-90d5-3a17e101f2c4");
-            var expectedItem = new Item
+            var itemBeforeUpdate = new Item
             {
                 Id = guid,
-                Text = updatedText,
+                Text = originalText,
                 CreatedAt = creationTime,
-                ModifiedAt = currentTime
+                ModifiedAt = creationTime
             };
             var itemToUpdate = new Item
             {
                 Id = guid,
                 Text = updatedText,
-                CreatedAt = creationTime,
-                ModifiedAt = creationTime
+                CreatedAt = creationTime
             };
+            var expectedItem = new RetrievedItem
+            {
+                WasFound = true,
+                Item = new Item
+                {
+                    Id = guid,
+                    Text = updatedText,
+                    CreatedAt = creationTime,
+                    ModifiedAt = currentTime
+                }
+            };
+            _getItemByIdService.GetItemByIdAsync(Arg.Any<Guid>()).Returns(new RetrievedItem { WasFound = true, Item = itemBeforeUpdate });
             _repository.UpdateAsync(Arg.Any<Guid>(), Arg.Is<Item>(value
-                => value.Id == expectedItem.Id
-                   && value.Text == expectedItem.Text
-                   && value.CreatedAt == expectedItem.CreatedAt
+                => value.Id == itemToUpdate.Id
+                   && value.Text == itemToUpdate.Text
+                   && value.CreatedAt == itemToUpdate.CreatedAt
                    && value.ModifiedAt == currentTime
-            )).Returns(expectedItem);
+            )).Returns(new Item
+            {
+                Id = guid,
+                Text = updatedText,
+                CreatedAt = creationTime,
+                ModifiedAt = currentTime
+            });
 
             var testItem = await _updateItemService.UpdateItemAsync(guid, itemToUpdate);
 
-            Assert.That(testItem, Is.EqualTo(expectedItem).UsingItemEqualityComparer());
+            Assert.That(testItem.Item, Is.EqualTo(expectedItem.Item).UsingItemEqualityComparer());
+            Assert.That(testItem.WasFound, Is.EqualTo(expectedItem.WasFound));
         }
     }
 }
