cb2736813e488c882ee515a2a93bc358d5c11e54
Matus Backor <MatusB@kentico.com>
2018-03-09 10:17:40 +0100

    KA-235 Refactor GetItemByIdServiceTests

--- TodoApp/test/TodoApp.Services.Tests/Services/GetItemByIdServiceTests.cs
+++ TodoApp/test/TodoApp.Services.Tests/Services/GetItemByIdServiceTests.cs
@@ -26,7 +26,8 @@ namespace TodoApp.Services.Tests.Services
         [Test]
         public async Task GetItemByIdAsync_ExistingId_ReturnsCorrectItem()
         {
-            var time = new DateTime(2018, 5, 4, 3, 2, 1);
+            var time = 
+                new DateTime(year: 2018, month: 5, day: 4, hour: 3, minute: 2, second: 1);
             var id = new Guid("44e506f7-c42b-42e5-823c-a5f1cf2e16f9");
             var item = new Item
             {
@@ -44,6 +45,37 @@ namespace TodoApp.Services.Tests.Services
             var testItem = await _getItemByIdService.GetItemByIdAsync(id);
 
             Assert.That(testItem.Item, Is.EqualTo(expectedItem.Item));
+        }
+
+        [Test]
+        public async Task GetItemByIdAsync_ExistingId_ReturnsRetrievedItemWithSuccess()
+        {
+            var time = 
+                new DateTime(year: 2018, month: 5, day: 4, hour: 3, minute: 2, second: 1);
+            var id = new Guid("44e506f7-c42b-42e5-823c-a5f1cf2e16f9");
+            var item = new Item
+            {
+                Id = id,
+                Text = "testText",
+                CreatedAt = time,
+                ModifiedAt = time
+            };
+            _repository.GetByIdAsync(id).Returns(item);
+
+            var testItem = await _getItemByIdService.GetItemByIdAsync(id);
+
+            Assert.That(testItem.WasFound, Is.True);
+        }
+
+        [Test]
+        public async Task GetItemByIdAsync_NonExistingId_ReturnsItemWithFalseStatus()
+        {
+            var id = new Guid("44e506f7-c42b-42e5-823c-a5f1cf2e16f9");
+            _repository.GetByIdAsync(id).Returns(null as Item);
+
+            var testItem = await _getItemByIdService.GetItemByIdAsync(id);
+
+            Assert.That(testItem.WasFound, Is.False);
         }
     }
 }
